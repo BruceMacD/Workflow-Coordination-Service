055e88c0c303bafa6bdbf98386a32f152f981c00
Bruce <brucewmacdonald@gmail.com>
2018-04-08 16:51:35 -0300

    update endpoints to prod

--- ui-bundle/src/_constants/server.constants.js
+++ ui-bundle/src/_constants/server.constants.js
@@ -1,14 +1,14 @@
 export const serverConstants = {
-    MBR_SERVER_LOGIN_ENDPOINT: 'http://localhost:8080/mbr/auth',
-    MBR_SERVER_CREATE_ENDPOINT: 'http://localhost:8080/mbr',
-    MBR_SERVER_CREATE_USER_ENDPOINT: 'http://localhost:8080/mbr/register',
-    MBR_SERVER_GET_STATUS_ENDPOINT: 'http://localhost:8080/mbr',
+    MBR_SERVER_LOGIN_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/mbr/auth',
+    MBR_SERVER_CREATE_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/mbr',
+    MBR_SERVER_CREATE_USER_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/mbr/register',
+    MBR_SERVER_GET_STATUS_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/mbr',
 
-    EMP_SERVER_CREATE_USER_ENDPOINT: 'http://localhost:8080/emp/register',
-    EMP_SERVER_LOGIN_ENDPOINT: 'http://localhost:8080/emp/auth',
-    EMP_SERVER_ROOT_ENDPOINT: 'http://localhost:8080/emp',
+    EMP_SERVER_CREATE_USER_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/emp/register',
+    EMP_SERVER_LOGIN_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/emp/auth',
+    EMP_SERVER_ROOT_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/emp',
 
-    RE_SERVER_CREATE_USER_ENDPOINT: 'http://localhost:8080/re/register',
-    RE_SERVER_LOGIN_ENDPOINT: 'http://localhost:8080/re/auth',
-    RE_APPRAISAL_ENDPOINT: 'http://localhost:8080/re',
+    RE_SERVER_CREATE_USER_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/re/register',
+    RE_SERVER_LOGIN_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/re/auth',
+    RE_APPRAISAL_ENDPOINT: 'http://cloundfinalserver-env.us-east-1.elasticbeanstalk.com/re',
 };
